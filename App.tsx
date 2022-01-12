66db573e05a4b285b6da4901c898b44995c345ef
Dylan Park <dylpark@protonmail.com>
2022-01-12 19:23:04 +1000

    Adding react navigation

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Signal Clone</Text>
-      <StatusBar style="auto" />
-    </View>
+
+    <NavigationContainer>{
+      <View style={styles.container}>
+        <Text>Signal Clone</Text>
+        <StatusBar style="auto" />
+      </View>
+    }</NavigationContainer>
+
   );
 }
 
