3b421bc972e1f0d870b96785fcf4cba5b7247bcd
Dylan Park <dylpark@protonmail.com>
2022-01-16 18:47:33 +1000

    Create LoginScreen.js

---
+++ screens/LoginScreen.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Button, Input, Image } from "react-native-elements";
+import { StatusBar } from "expo-status-bar";
+
+const LoginScreen = () => {
+  return (
+    <View>
+      <StatusBar style="light" />
+      <Image
+        source={{
+          uri: "https://blog.mozilla.org/internetcitizen/files/2018/08/signal-logo.png",
+        }}
+        style={{ width: 200, height: 200 }}
+      />
+
+      <View style={styles.inputContainer}>
+        <Input placeholder="Email" autoFocus type="email" />
+        <Input placeholder="Password" secureTextEntry type="password" />
+      </View>
+    </View>
+  );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {},
+});
