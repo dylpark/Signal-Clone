75ed19aa757c7a318e4f16856450110f9528bc0f
Dylan Park <dylpark@protonmail.com>
2022-01-16 18:00:13 +1000

    Adding Signal icon to login screen

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import { StyleSheet, Text, View, } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Button, Input, Image } from "react-native-elements";
+import { StatusBar } from "expo-status-bar";
 
 const LoginScreen = () => {
-    return (
-        <View>
-            <Text>Login</Text>
-        </View>
-    )
-}
+  return (
+    <View>
+      <StatusBar style="light" />
+      <Text>Login Screen</Text>
+      <Image source={{
+        uri: "https://blog.mozilla.org/internetcitizen/files/2018/08/signal-logo.png",
+      }}
+        style={{ width: 200, height: 200 }} />
+    </View>
+  );
+};
 
-export default LoginScreen
+export default LoginScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
