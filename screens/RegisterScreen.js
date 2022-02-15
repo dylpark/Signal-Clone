b4521bbfee3e1b8a1dcc95fde2ebfdec57acd54b
Dylan Park <dylpark@protonmail.com>
2022-02-15 20:31:07 +1000

    Update RegisterScreen.js

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,7 +1,7 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { KeyboardAvoidingView } from "react-native";
-import { Button, Input, Image } from "react-native-elements";
+import { Button, Input, Image, Text } from "react-native-elements";
 import React, { useState } from "react";
 
 const RegisterScreen = ({ navigation }) => {
@@ -15,9 +15,11 @@ const RegisterScreen = ({ navigation }) => {
   return (
     <KeyboardAvoidingView behavior="padding" style={styles.container}>
       <StatusBar style="light"></StatusBar>
+
       <Text h3 style={{ marginBottom: 50 }}>
         Create a Signal account
       </Text>
+
       <View style={styles.inputContainer}>
         <Input
           placeholder="Full Name"
@@ -26,12 +28,14 @@ const RegisterScreen = ({ navigation }) => {
           value={name}
           onChangeText={(text) => setName(text)}
         />
+
         <Input
           placeholder="Email"
           type="text"
           value={email}
           onChangeText={(text) => setEmail(text)}
         />
+
         <Input
           placeholder="Password"
           type="text"
@@ -39,6 +43,7 @@ const RegisterScreen = ({ navigation }) => {
           value={password}
           onChangeText={(text) => setPassword(text)}
         />
+
         <Input
           placeholder="Profile Picture URL (optional)"
           type="imageUrl"
@@ -47,12 +52,35 @@ const RegisterScreen = ({ navigation }) => {
           onSubmitEditing={register}
         />
       </View>
+
+      <Button
+        containerStyle={styles.button}
+        raised
+        onPress={register}
+        title="Register"
+      />
+      <View style={{ height: 100 }} />
     </KeyboardAvoidingView>
   );
 };
 
+// cmd + ctrl + z for expo menu in simulator.
+
 export default RegisterScreen;
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+    backgroundColor: "white",
+  },
+  button: {
+    width: 200,
+    marginTop: 10,
+  },
+  inputContainer: {
+    width: 300,
+  },
 });
