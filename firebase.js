cf6d6e67c8a0b28fd139d0163cb954c357415e0e
Dylan Park <dylpark@protonmail.com>
2022-03-19 19:43:31 +1000

    Clean rebuild of project

---
+++ firebase.js
@@ -0,0 +1,27 @@
+// import * as firebase from "firebase";
+// import "firebase/auth";
+// import "firebase/database";
+// //import {...} from "firebase/firestore";
+// //import {...} from "firebase/functions";
+// //import {...} from "firebase/storage";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyByMVaDmsy7rfv2G9jYqhisrJ95_G_gwi8",
+//   authDomain: "signal-clone-19e3f.firebaseapp.com",
+//   projectId: "signal-clone-19e3f",
+//   storageBucket: "signal-clone-19e3f.appspot.com",
+//   messagingSenderId: "490106179429",
+//   appId: "1:490106179429:web:4567c44f6b1d4a240e1b8c",
+// };
+
+// let app;
+
+// if (firebase.apps.length === 0) {
+//   app = firebase.initializeApp(firebaseConfig);
+// } else {
+//   app = firebase.app();
+// }
+
+// const db = app.firestore();
+// const auth = firebase.auth();
+// export { db, auth };
