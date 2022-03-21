47a486477f43338a556384d2e08504e2369008c4
Dylan Park <dylpark@protonmail.com>
2022-03-21 20:57:52 +1000

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -9,7 +9,7 @@ import {
 } from "react-native";
 import { Avatar } from "react-native-elements";
 import { AntDesign, SimpleLineIcons } from "@expo/vector-icons";
-import { auth, db } from "../firebase";
+import { auth, db } from "./firebase";
 
 const HomeScreen = ({ navigation }) => {
   const [chats, setChats] = useState([]);
@@ -21,6 +21,12 @@ const HomeScreen = ({ navigation }) => {
   };
 
   useLayoutEffect(() => {});
+
+  return (
+    <SafeAreaView>
+      <Text>This is home screen</Text>
+    </SafeAreaView>
+  );
 };
 
 export default HomeScreen;
